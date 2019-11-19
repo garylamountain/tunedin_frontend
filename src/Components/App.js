bddaf9ddbc5ec57d189a5f8c554fbeaad4865a28
github username <github email address>
2019-11-19 15:10:42 -0600

    added hardcoded iframe

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,18 +10,33 @@ class App extends Component {
   
   state = {
     currentUser: {},
-    playlists: {}
+    nowSelected: {},
+    nowPlaying: {},
+    queriedPlaylists: []
   }
 
-  renderProfile = () => {
-    return <Profile currentUser = {this.state.currentUser} handleClick={this.handleClick}/>
-  } 
+  // renderProfile = () => {
+  //   return <Profile currentUser = {this.state.currentUser} handleClick={this.handleClick}/>
+  // } 
 
   handleCode = (code) =>{
     Auth.login(code)
       .then(res=>{
         const currentUser = res
-        this.setState({currentUser},this.props.history.push("/profile"))
+        this.setState({currentUser},this.props.history.push("/"))
+      })
+  }
+
+  componentDidMount(){
+      fetch('http://localhost:3000/playlists')
+      .then(res => res.json())
+      .then(data => {
+        let nowSelected = {};
+        let pop = data.filter(genre => genre.name === 'pop');
+        nowSelected["name"] = pop[0].name;
+        nowSelected["url"] = pop[0].url;
+        let nowPlaying = nowSelected
+        this.setState({nowSelected,nowPlaying})
       })
   }
 
@@ -29,18 +44,54 @@ class App extends Component {
     return <Callback location={location} handleCode={this.handleCode} />
   }
 
-  handleClick = () => {
-    console.log("HIIII")
-  }
+  // handleClick = () => {
+  //   console.log("HIIII")
+  // }
 
   render() {
+    // const rp = require('request-promise');
+    // const $ = require('cheerio');
+    // const url = 'http://everynoise.com/everynoise1d.cgi?root=pop&scope=all';
+    // rp(url)
+    // .then(function(html){
+    //   //success!
+    //   console.log($('big > a', html).length);
+    // })
+    // .catch(function(err){
+    //   //handle error
+    // });
+    fetch("http://everynoise.com/everynoise1d.cgi?root=pop&scope=all",{
+      mode: 'no-cors'
+      // headers: {
+      //   'Access-Control-Allow-Origin':'*'
+      // }
+    })
+    .then(res => console.log(res))
+    // .then(data => console.log(data))
+    console.log(this.state)
     return (
       <div className="App">
         <h1>SPOTIFY API</h1>
         <br/>
-        <Login currentUser={this.state.currentUser}/>
+        {this.state.currentUser.display_name ? 
+        <div>
+          {this.state.nowSelected.name ? <h6>Now Select: {this.state.nowSelected.name}</h6> : null}
+          {this.state.nowPlaying.name ? <h6>Now Playing: {this.state.nowPlaying.name}</h6> : null}
+          <Profile currentUser = {this.state.currentUser} handleClick={this.handleClick}/>
+          {this.state.nowPlaying.name ?
+          <iframe src={'https://open.spotify.com/embed/' + this.state.nowPlaying.url.replace("spotify:","").replace(":","/")} width="300" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+          : 
+          null}
+        </div>
+        
+        : 
+        <div>
+          <p>Not Logged In</p>
+          <Login currentUser={this.state.currentUser}/>
+        </div>
+        }
         <Route exact path="/callback" component={this.handleCallback} />
-        <Route exact path="/profile" component={this.renderProfile} />
+        {/* <Route exact path="/" component={this.renderProfile} /> */}
       </div>
     );
   }
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,23 +1,14 @@
 import React from 'react';
 
 const Profile = props => {
-   fetch("https://api.spotify.com/v1/me/playlists", {
-    headers: {
-      'Authorization': 'Bearer ' + props.currentUser.access_token
-    }}
-  )
-  .then(res => res.json())
-  .then(data => {
-      let playlists = data.items
-  })
    return(
        <div>
            <h1>{props.currentUser.display_name}</h1>
-           <h1>{props.currentUser.email}</h1>
+           {/* <h1>{props.currentUser.email}</h1> */}
            <a href={props.currentUser.spotify_url}>
             <img src={props.currentUser.profile_img_url} alt="spotifyImg"/>
            </a>
-           <button onClick={props.handleClick}>TEST</button>
+           {/* <button onClick={props.handleClick}>TEST</button> */}
        </div>
    )
 }
