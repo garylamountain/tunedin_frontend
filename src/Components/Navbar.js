0a4ae14b53f156a1b30160264aa03026b0a268fd
github username <github email address>
2019-11-26 10:30:55 -0600

    adjusted search bar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,6 +13,9 @@ class Navbar extends React.Component {
     handleChange = e => {
         this.setState({value: e.target.value});
         this.props.clearSearchArray();
+        if(this.state.value.length > 2){
+            this.props.searchPlaylists(this.state.value)
+        }
     }
 
     search = e => {
@@ -25,52 +28,60 @@ class Navbar extends React.Component {
 
     clearSearch = () => {
         this.setState({value: ""})
-        this.props.clearSearchArray()
+        // this.props.clearSearchArray()
     }
 
     render(){
         return (
-            <div>
                 <nav className="navbar">  
-                <div className="dropdown">
-                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Jump
-                </button>        
-                    <div className="dropdown-content">
-                        {this.props.queriedPlaylists && this.props.queriedPlaylists != [] ?
-                        this.props.queriedPlaylists.slice(2,this.props.queriedPlaylists.length - 1).map(item => this.props.renderRelatedPlaylists(item))
-                        :
-                        null}
+                    <div className="row">
+                        <div className="col-3">
+                            <div className="dropdown">
+                                <div className="float-left">
+                                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Jump
+                                    </button>  
+                                    <div className="dropdown-content">
+                                        {this.props.queriedPlaylists && this.props.queriedPlaylists != [] ?
+                                        this.props.queriedPlaylists.slice(2,this.props.queriedPlaylists.length - 1).map(item => this.props.renderRelatedPlaylists(item))
+                                        :
+                                        null}
+                                    </div>
+                                </div>      
+                            </div>
+                        </div>
+                        <div className="col-6">
+                            <h5 className="logo">TunedIn</h5>
+                        </div>
+                        <div className="col-2">
+                            <div onBlur={this.clearSearch}>
+                                <input value={this.state.value}  type="text" placeholder="Search" aria-label="Search" onChange={this.handleChange}/>
+                                <ul id="navbar-results">{this.props.searchArray.map(item => this.props.renderSearch(item))}</ul>
+                                {/* <button onClick={this.search} aria-hidden="true">Search</button> */}
+                            </div>
+                            {this.props.currentUser.profile_img_url ? 
+                            <div className="col-2">
+                            
+                            <a href={this.props.currentUser.spotify_url}>
+                                <img className="profile-picture" src={this.props.currentUser.profile_img_url} alt="spotifyImg"/>
+                            </a>
+                            <div className="dropdown">
+                            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            ❤️
+                            </button>        
+                                <div className="dropdown-content">
+                                    {this.props.savedPlaylists && this.props.savedPlaylists != [] ?
+                                    this.props.savedPlaylists.map(item => this.props.renderSavedPlaylists(item))
+                                    :
+                                    null}
+                                </div>
+                            </div>
+                            </div>
+                            :
+                            null}
+                        </div>
                     </div>
-                </div>
-                <h5 className="logo">TunedIn</h5>
-                <div>
-                    <input value={this.state.value} className="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search" aria-label="Search" onChange={this.handleChange}/>
-                    <button onClick={this.search} aria-hidden="true">Search</button>
-                    <ul id="navbar-results">{this.props.searchArray.map(item => this.props.renderSearch(item))}</ul>
-                </div>
-                {this.props.currentUser.profile_img_url ? 
-                <div>
-                <div className="dropdown">
-                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                ❤️
-                </button>        
-                    <div className="dropdown-content">
-                        {this.props.savedPlaylists && this.props.savedPlaylists != [] ?
-                        this.props.savedPlaylists.map(item => this.props.renderSavedPlaylists(item))
-                        :
-                        null}
-                    </div>
-                </div>
-                <a href={this.props.currentUser.spotify_url}>
-                    <img className="profile-picture" src={this.props.currentUser.profile_img_url} alt="spotifyImg"/>
-                </a>
-                </div>
-                :
-                null
-                }
                 </nav>
-            </div>
         )
     }
 }
